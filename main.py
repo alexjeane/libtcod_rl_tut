80f3dbe4f9539b1dcdb04375f8e4c025bcbb1159
Alex Jeane <alex@alexjeane.com>
2022-08-10 21:42:28 -0500

    Incorporated entity into main class and initialized both the player and a new NPC.

--- main.py
+++ main.py
@@ -9,9 +9,6 @@ from input_handlers import EventHandler
 def main() -> None:
     screen_width = 80
     screen_height = 50
-
-    player_x = int(screen_width / 2 )
-    player_y = int(screen_height / 2)
     
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -19,6 +16,10 @@ def main() -> None:
 
     event_handler = EventHandler()
 
+    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
+    entities = {npc, player}
+    
     with tcod.context.new_terminal(
         screen_width,
         screen_height,
