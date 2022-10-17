08cf3f8eaaa68f6849774470f2adaf417d20704e
Alex Jeane <alex@alexjeane.com>
2022-10-16 20:54:59 -0500

    Added in separate class for ActionWithDirection and call it from MovementAction.

--- actions.py
+++ actions.py
@@ -23,22 +23,26 @@ class EscapeAction(Action):
     def perform(self, engine: Engine, entity: Entity) -> None:
         raise SystemExit()
 
-class MovementAction(Action):
+class ActionWithDirection(Action):
     def __init__(self, dx: int, dy: int):
         super().__init__()
 
-
         self.dx = dx
         self.dy = dy
+
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        raise NotImplementedError()
+class MovementAction(ActionWithDirection):
 
     def perform(self, engine: Engine, entity: Entity) -> None:
         dest_x = entity.x + self.dx
         dest_y = entity.y + self.dy
         
         if not engine.game_map.in_bounds(dest_x, dest_y):
-            return # Destination is out of bounds.
-        
+            return # Destination is out of bounds.        
         if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
             return # Destination is blocked by a tile.
+        if engine.game_map.get_block_entity_at_location(dest_x, dest_y):
+            return  # Destination is blocked by an entity.
 
         entity.move(self.dx, self.dy)
