aaff0dc9ee1c0520c29c0ace68eeac9f32273d42
Alex Jeane <alex@alexjeane.com>
2022-12-03 20:47:57 -0600

    Added the action to equip items to an actor.

--- actions.py
+++ actions.py
@@ -84,6 +84,16 @@ class DropItem(ItemAction):
         return self.entity.inventory.drop(self.item)
 
 
+class EquipAction(Action):
+    def __init__(self, entity: Actor, item: Item):
+        super().__init__(entity)
+
+        self.item = item
+
+    def perform(self) -> None:
+        self.entity.equipment.toggle_equip(self.item)
+
+
 class WaitAction(Action):
     def perform(self) -> None:
         pass
